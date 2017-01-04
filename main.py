775eb05adbc86a9b7e085db7fb54a2cd94183a4e
Иван Кузнецов < >
2017-01-04 13:56:18 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from PIL import Image
+print("Введите путь к картинке, к которой вы хотите применить фильтр.")
+im = Image.open(input())
+print("Выполняю. Ждите")
+snow = Image.open('snow.png')
+hat = Image.open('hat.png')
+rides = Image.open('santarides.png')
+pixels = im.load()
+imWidth = im.width
+imHeight = im.height
+
+snow = snow.resize((imWidth, imHeight))
+hat = hat.resize((int(imWidth * 0.1), int(imHeight * 0.1)))
+rides = rides.resize((int(imWidth * 0.3), int(imHeight * 0.3)))
+
+
+im.paste(snow, (0,0), snow)
+
+for i in range(im.width):
+    for j in range(im.height):
+        r, g, b = pixels[i, j]
+
+        a = (r + g + b) // 3
+        if a > 235:
+            r = r - r * 0.2
+            g = g - g * 0.2
+            b = b - b * 0.2
+        r = min(255, int(r * 1.1))
+        g = min(255, int(g * 1.2))
+        b = min(255, int(b * 1.55))
+        r = min(255, int(r * 1.1))
+        g = min(255, int(g * 1.1))
+        b = min(255, int(b * 1.1))
+        pixels[i, j] = (r, g, b)
+
+im.paste(hat, (im.width - hat.width, 0), hat)
+im.paste(hat, (im.width // 2 - hat.width // 2, 0), hat)
+im.paste(hat, (0, 0), hat)
+im.paste(rides, (0, im.height - rides.height), rides)
+im.show()
+##print("Фильтр применен! Введите место сохранения картинки.")
+##im.save(input())
