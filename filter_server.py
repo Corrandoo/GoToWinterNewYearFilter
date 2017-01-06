e55ebd2af473857e684f9dc5a5d0d233abc5df23
Corrandoo <ivan260302@yandex.ru>
2017-01-06 14:00:55 +0300

    Started working on web

--- filter_server.py
+++ filter_server.py
@@ -1,9 +1,23 @@
 import tornado.ioloop
 import tornado.web
+import os
+import uuid
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('upload.html')
+    def post(self):
+        fileinfo = self.request.files['image'][0]
+        fname = fileinfo['filename']
+        extn = os.path.splitext(fname)[1]
+        cname = str(uuid.uuid4()) + extn
+        fh = open('images/' + cname, 'w')
+        fh.write(fileinfo['body'])
+        #обработка
+        fh = open('results/' + cname, 'w')
+        fh.write(fileinfo['body'])
+        self.render('result.html', name=cname)
+
 
 settings = [
     ('/', MainHandler),
