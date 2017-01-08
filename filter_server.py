0a186af428e1992ecb83c4203c85618afb7810ac
Corrandoo <ivan260302@yandex.ru>
2017-01-08 12:05:04 +0300

    Added protection

--- filter_server.py
+++ filter_server.py
@@ -12,7 +12,7 @@ class MainHandler(tornado.web.RequestHandler):
         full_name = ""
         if name=='default':
             newname = str(uuid.uuid4())
-            self.render('upload.html', name=newname)
+            self.render('upload.html', name=newname, error_report = "")
         else:
             list = listdir('results')
             for filename in list:
@@ -21,30 +21,33 @@ class MainHandler(tornado.web.RequestHandler):
 
             self.render('result.html', name=full_name) #full name - полное имя файла
     def post(self):
-        name = self.get_argument('c')
-        fileinfo = self.request.files['image'][0]
-        fname = fileinfo['filename']
-        extn = os.path.splitext(fname)[1]
-        cname = name + extn
-        fh = open('images/' + cname, 'wb')
-        fh.write(fileinfo['body'])
-        #обработка
+        try:
+            name = self.get_argument('c', 'default')
+            fileinfo = self.request.files['image'][0]
+            fname = fileinfo['filename']
+            extn = os.path.splitext(fname)[1]
+            cname = name + extn
+            fh = open('images/' + cname, 'wb')
+            fh.write(fileinfo['body'])
+            #обработка
 
-        type = self.get_argument('filter', "default")
-        if type == "filterik":
-            processik('images/' + cname, 'results/' + cname)
-        elif type == "filterym":
-            processym('images/' + cname, 'results/' + cname)
-        elif type == "filterdb":
-            processdb('images/' + cname, 'results/' + cname)
-        elif type == "filtervc":
-            processvc('images/' + cname, 'results/' + cname)
-        elif type == "filtermm1":
-            processmm1('images/' + cname, 'results/' + cname)
-        elif type == "filtermm2":
-            processmm2('images/' + cname, 'results/' + cname)
+            type = self.get_argument('filter', "default")
+            if type == "filterik":
+                processik('images/' + cname, 'results/' + cname)
+            elif type == "filterym":
+                processym('images/' + cname, 'results/' + cname)
+            elif type == "filterdb":
+                processdb('images/' + cname, 'results/' + cname)
+            elif type == "filtervc":
+                processvc('images/' + cname, 'results/' + cname)
+            elif type == "filtermm1":
+                processmm1('images/' + cname, 'results/' + cname)
+            elif type == "filtermm2":
+                processmm2('images/' + cname, 'results/' + cname)
 
-        self.render('result.html', name=cname)
+            self.render('result.html', name=cname)
+        except:
+            self.render('upload.html', name=str(uuid.uuid4()), error_report="Загрузите файл перед отправкой его в фильтр!")
 
 
 settings = [
